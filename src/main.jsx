5b7e0e2f88de2bc27564e3b144316d419176c3ff
87009523ks-coder <87009523ks@gmail.com>
2026-05-11 08:55:19 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App' // 확장자 없이 호출하여 에러 방지
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
