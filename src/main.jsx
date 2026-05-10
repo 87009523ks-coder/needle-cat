30f705118e1df52923c8b9f5974ab6cc5346cf95
87009523ks-coder <87009523ks@gmail.com>
2026-05-10 19:16:38 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// index.html에 있는 <div id="root"></div>에 우리 앱을 그려주는 역할을 합니다.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
